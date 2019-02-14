576925136f466fed89ae59bdcf41eb74ee6ff819
Vladimir Kazarin <vladimir_kazarin@yahoo.com>
2019-02-14 13:49:56 -0500

    finishing tcp client/server

--- echo-client.py
+++ echo-client.py
@@ -1,4 +1,4 @@
-import socket
+import socket, struct
 from datetime import datetime
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
@@ -7,8 +7,10 @@ PORT = 65432        # The port used by the server
 before_call  = datetime.now()
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
     data = s.recv(1024)
+    received_time = struct.unpack('f', data)[0]
+    received_datetime = datetime.fromtimestamp(received_time)
+    print('Received datetime: ', received_datetime)
 after_call = datetime.now()
 milsec = after_call - before_call
 print(f'it took {milsec} to fetch {data}')
--- echo-server.py
+++ echo-server.py
@@ -1,16 +1,15 @@
-import socket
+import socket, struct, time 
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen(0)
+    s.listen()
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
         while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+            time_now = time.time()
+            if time_now:
+                conn.send(struct.pack('f',time_now))
