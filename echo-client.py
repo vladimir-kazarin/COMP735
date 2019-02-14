76fdc471774e831c3c9bcf142d3cc78ecb80af3e
Vladimir Kazarin <vladimir_kazarin@yahoo.com>
2019-02-14 09:40:27 -0500

    adding datatime to the client

--- echo-client.py
+++ echo-client.py
@@ -1,11 +1,14 @@
 import socket
+from datetime import datetime
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
+before_call  = datetime.now()
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'Hello, world')
     data = s.recv(1024)
-
-print('Received', repr(data))
+after_call = datetime.now()
+milsec = after_call - before_call
+print(f'it took {milsec} to fetch {data}')
